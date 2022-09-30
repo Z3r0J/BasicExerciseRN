61a7c9421a9feefc6c5baa431037c4d5c90c115d
Jean Carlos Reyes Encarnación <20210546@itla.edu.do>
2022-09-30 01:29:20 -0400

    Inicio Component ready!

--- src/Components/HomeComponent/HomeComponent.tsx
+++ src/Components/HomeComponent/HomeComponent.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Image,Text,useColorScheme } from 'react-native'
+import { receiveTheme } from '../../helpers/Styles';
+import { AboutMe } from '../../Logic/AboutMe'
+import IonIcon from 'react-native-vector-icons/Ionicons';
+
 
 export const HomeComponent = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
   return (
-    <View><Text>Home</Text></View>
+    <View style={{padding:"3%"}}>
+      <Image source={require('../../../assets/images/85190332.jpg')} style={{width:"100%",height:'65%'}}/>
+      {AboutMe.map((me)=>{
+        return(
+          <View style={receiveTheme(isDarkMode).listStyle}>
+            <Text key={me.name} style={receiveTheme(isDarkMode).item}><IonIcon name='information-circle-outline' size={22} color={isDarkMode?'#0ECB9D':'#0DC195'}/>  Nombre: {me.name}</Text>
+            <Text key={me.enroll} style={receiveTheme(isDarkMode).item}><IonIcon name='information-circle-outline' size={22} color={isDarkMode?'#0ECB9D':'#0DC195'}/>  Matricula: {me.enroll}</Text>
+            <Text key={me.teacher} style={receiveTheme(isDarkMode).item}><IonIcon name='information-circle-outline' size={22} color={isDarkMode?'#0ECB9D':'#0DC195'}/>  Profesor: {me.teacher}</Text>
+          </View>
+        )
+      })}
+    </View>
   )
 }
---
+++ src/Logic/AboutMe.js
@@ -0,0 +1,5 @@
+export const AboutMe = [{
+    name: "Jean Carlos Reyes",
+    enroll:"2021-0546",
+    teacher:"Amadis Suarez Genao"
+}]
